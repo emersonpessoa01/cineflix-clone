6a255b60b10330f910c4d705b41797e6a5143e7d
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2024-02-24 18:31:18 -0300

    added scroll-top :sparkles:

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,41 @@
-'use strict';
+"use strict";
 
 // variables for navbar menu toggle
-const header = document.querySelector('header');
-const nav = document.querySelector('nav');
-const navbarMenuBtn = document.querySelector('.navbar-menu-btn');
+const header = document.querySelector("header");
+const nav = document.querySelector("nav");
+const navbarMenuBtn = document.querySelector(".navbar-menu-btn");
 
 // variables for navbar search toggle
-const navbarForm = document.querySelector('.navbar-form');
-const navbarFormCloseBtn = document.querySelector('.navbar-form-close');
-const navbarSearchBtn = document.querySelector('.navbar-search-btn');
-
+const navbarForm = document.querySelector(".navbar-form");
+const navbarFormCloseBtn = document.querySelector(".navbar-form-close");
+const navbarSearchBtn = document.querySelector(".navbar-search-btn");
 
 // navbar menu toggle function
 function navIsActive() {
-  header.classList.toggle('active');
-  nav.classList.toggle('active');
-  navbarMenuBtn.classList.toggle('active');
+  header.classList.toggle("active");
+  nav.classList.toggle("active");
+  navbarMenuBtn.classList.toggle("active");
 }
 
-navbarMenuBtn.addEventListener('click', navIsActive);
-
-
+navbarMenuBtn.addEventListener("click", navIsActive);
 
 // navbar search toggle function
-const searchBarIsActive = () => navbarForm.classList.toggle('active');
+const searchBarIsActive = () => navbarForm.classList.toggle("active");
 
-navbarSearchBtn.addEventListener('click', searchBarIsActive);
-navbarFormCloseBtn.addEventListener('click', searchBarIsActive);
+navbarSearchBtn.addEventListener("click", searchBarIsActive);
+navbarFormCloseBtn.addEventListener("click", searchBarIsActive);
+
+//activate button scrolltop
+
+const scrollTop = document.querySelector(".scroll-top");
+
+window.onscroll = () => {
+  scrollTop.classList.toggle("active", window.scrollY > 0);
+};
+
+scrollTop.onclick = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
